63c71adeffd5b0eca4fc5133418c91d08c110bce
Emilio M <emiliomendozareyes@gmail.com>
2024-02-12 15:02:47 -0500

    Implemented realloc

--- include/stddef.h
+++ include/stddef.h
@@ -8,7 +8,7 @@ typedef _mlibc_ptrdiff_t ssize_t;
 // nullptr is a C23 feature
 // gcc 13 started supporting it
 
-#if __STDC_VERSION__ >= 201112L
+#if (__STDC_VERSION__ >= 201112L || __cplusplus >= 201103L) && __GNUC__ >= 13 // TODO: add clang version
 #define NULL nullptr
 #else
 #define NULL ((void*)0)
